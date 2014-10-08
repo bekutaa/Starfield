ee32ce1a292fe0e7446337cad7ddfe56482b11f7
bekutaa <erika.auyeung@sbcglobal.net>
2014-10-08 10:44:39 -0700

    Edited program and html.

--- Starfield.pde
+++ Starfield.pde
@@ -1,16 +1,24 @@
 int frameCount = 0;
+boolean trail = true;
 
 Particle [] ichi;
+
+interface Particle
+{
+	public void redo();
+	public void move();
+	public void show();
+}
 
 void setup()
 {
 	size(600,600);
 	background(0);
 
-	ichi = new Particle[101];
+	ichi = new Particle[301];
 	for(int i = 0; i < ichi.length; i++)
 	{
-		if(i == 0)
+		if(i == ichi.length-1)
 		{
 			ichi[i] = new OddballParticle();
 		}
@@ -20,26 +28,51 @@ void setup()
 		}
 	}
 }
+
 void draw()
 {
-	if( !(frameCount > 5))
+	frameCount++;
+
+	if(trail)
 	{
-		frameCount++;
+		fill(0,0,0,12);
+		rect(-10,-10,width+100,height+100);
+	}
+	else
+	{
+		background(0);
 	}
 
-	fill(0,0,0,12);
-	rect(-10,-10,width+100,height+100);
 	for(int i = 0; i < ichi.length; i++)
 	{
 		ichi[i].move();
 		ichi[i].show();
 	}
 }
+
+void keyPressed()
+{
+	if(key == 'a')
+	{
+		trail = !trail;
+	}
+
+	if(key == 's')
+	{
+		for(int i = 0; i < ichi.length; i++)
+		{
+
+			ichi[i].redo();
+		}
+	}
+}
+
 class NormalParticle implements Particle
 {
 	double myX, myY, mySpeed, myAngle;
 	int myColor;
 	boolean orientation;
+	boolean swirl;
 
 	NormalParticle()
 	{
@@ -50,6 +83,22 @@ class NormalParticle implements Particle
 		mySpeed = Math.random()*4;
 		myAngle = Math.random()*2*Math.PI;
 		orientation = false;
+		swirl = true;
+	}
+
+	public void redo()
+	{
+		myColor = color((int)(Math.random()*255),(int)(Math.random()*255),(int)(Math.random()*255));
+		myX = width/2;
+		myY = height/2;
+
+		mySpeed = Math.random()*4;
+		myAngle = Math.random()*2*Math.PI;
+		swirl = !swirl;
+		if(swirl)
+		{
+			orientation = !orientation;
+		}
 	}
 
 	public void move()
@@ -62,16 +111,20 @@ class NormalParticle implements Particle
 			if(dist( (float)myX,(float)myY, 300,300) <= mySpeed/2)
 			{
 				orientation = !orientation;
+				// swirl = !swirl;
 			}
 		}
-
-		if(orientation)
-		{
-			myAngle -= 0.02;
-		}
-		else 
-		{
-			myAngle += 0.02;	
+		
+		if(swirl)
+	    {
+	    	if(orientation)
+			{
+				myAngle -= 0.02;
+			}
+			else 
+			{		
+				myAngle += 0.02;	
+			}
 		}
 	}
 
@@ -83,15 +136,10 @@ class NormalParticle implements Particle
 	}
 }
 
-interface Particle
-{
-	public void move();
-	public void show();
-}
-
 class OddballParticle implements Particle
 {
 	double myX, myY, mySpeed, myAngle;
+	double oldX, oldY;
 	int myColor;
 
 	OddballParticle()
@@ -99,36 +147,47 @@ class OddballParticle implements Particle
 		myColor = color(255,255,255);
 		myX = width/2;
 		myY = height/2;
-		mySpeed = (Math.random()*8)+(Math.random()*3);
+		mySpeed = (Math.random()*3)+(Math.random()*4);
 		myAngle = Math.random()*2*Math.PI;
+	}
+
+	public void redo()
+	{
+
 	}
 
 	public void move()
 	{
-		if(Math.random() > 0.5)
+		oldX = myX;
+		oldY = myY;
+
+		myX += (2*(Math.cos(myAngle) * mySpeed)) - (Math.cos(myAngle) * mySpeed);
+		myY += (2*(Math.sin(myAngle) * mySpeed)) - (Math.sin(myAngle) * mySpeed);
+
+		if(frameCount%15 == 0)
 		{
-			myX += ( Math.cos(myAngle) * mySpeed);
+			myAngle = Math.random()*2*Math.PI;
 		}
-		else
+
+		if(myX > width || myX < 0 )
 		{
-			myX -= ( Math.cos(myAngle) * mySpeed);
+			myAngle = Math.PI - myAngle;
 		}
-		if(Math.random() > 0.5)
+		if(myY > height || myY < 0)
 		{
-			myY += ( Math.sin(myAngle) * mySpeed);
+			myAngle = -1 * myAngle;
 		}
-		else
-		{
-			myY -= ( Math.sin(myAngle) * mySpeed);
-		}
-		myAngle = Math.random()*2*Math.PI;
 	}
 
 	public void show()
 	{
 		noStroke();
 		fill(myColor);
-		ellipse((float)myX,(float)myY,7,7);
+		ellipse((float)myX,(float)myY,10,10);
+
+		// strokeWeight(10);
+		// stroke(myColor);
+		// line((float)oldX,(float)oldY,(float)myX,(float)myY);
 	}
 }
 
--- build-tmp/source/Starfield.java
+++ build-tmp/source/Starfield.java
@@ -15,18 +15,26 @@ import java.io.IOException;
 public class Starfield extends PApplet {
 
 int frameCount = 0;
+boolean trail = true;
 
 Particle [] ichi;
+
+interface Particle
+{
+	public void redo();
+	public void move();
+	public void show();
+}
 
 public void setup()
 {
 	size(600,600);
 	background(0);
 
-	ichi = new Particle[101];
+	ichi = new Particle[301];
 	for(int i = 0; i < ichi.length; i++)
 	{
-		if(i == 0)
+		if(i == ichi.length-1)
 		{
 			ichi[i] = new OddballParticle();
 		}
@@ -36,34 +44,51 @@ public void setup()
 		}
 	}
 }
+
 public void draw()
 {
-	if( !(frameCount > 5))
+	frameCount++;
+
+	if(trail)
 	{
-		frameCount++;
+		fill(0,0,0,12);
+		rect(-10,-10,width+100,height+100);
+	}
+	else
+	{
+		background(0);
 	}
 
-	fill(0,0,0,12);
-	rect(-10,-10,width+100,height+100);
 	for(int i = 0; i < ichi.length; i++)
 	{
 		ichi[i].move();
 		ichi[i].show();
-
-		// if(frameCount > 5)
-		// {
-		// 	if(dist( (float)ichi[i].myX,(float)ichi[i].myY, 300,300) <= ichi[i].mySpeed/2)
-		// 	{
-		// 		println("ichi[" + i + "] turn");
-		// 	}
-		// }
 	}
 }
+
+public void keyPressed()
+{
+	if(key == 'a')
+	{
+		trail = !trail;
+	}
+
+	if(key == 's')
+	{
+		for(int i = 0; i < ichi.length; i++)
+		{
+
+			ichi[i].redo();
+		}
+	}
+}
+
 class NormalParticle implements Particle
 {
 	double myX, myY, mySpeed, myAngle;
 	int myColor;
 	boolean orientation;
+	boolean swirl;
 
 	NormalParticle()
 	{
@@ -74,6 +99,22 @@ class NormalParticle implements Particle
 		mySpeed = Math.random()*4;
 		myAngle = Math.random()*2*Math.PI;
 		orientation = false;
+		swirl = true;
+	}
+
+	public void redo()
+	{
+		myColor = color((int)(Math.random()*255),(int)(Math.random()*255),(int)(Math.random()*255));
+		myX = width/2;
+		myY = height/2;
+
+		mySpeed = Math.random()*4;
+		myAngle = Math.random()*2*Math.PI;
+		swirl = !swirl;
+		if(swirl)
+		{
+			orientation = !orientation;
+		}
 	}
 
 	public void move()
@@ -86,16 +127,20 @@ class NormalParticle implements Particle
 			if(dist( (float)myX,(float)myY, 300,300) <= mySpeed/2)
 			{
 				orientation = !orientation;
+				// swirl = !swirl;
 			}
 		}
-
-		if(orientation)
-		{
-			myAngle -= 0.02f;
-		}
-		else 
-		{
-			myAngle += 0.02f;	
+		
+		if(swirl)
+	    {
+	    	if(orientation)
+			{
+				myAngle -= 0.02f;
+			}
+			else 
+			{		
+				myAngle += 0.02f;	
+			}
 		}
 	}
 
@@ -105,18 +150,12 @@ class NormalParticle implements Particle
 		fill(myColor);
 		ellipse((float)myX,(float)myY,5,5);
 	}
-
-} //the pink bar was bothering me
-
-interface Particle
-{
-	public void move();
-	public void show();
-} //the pink bar was bothering me
+}
 
 class OddballParticle implements Particle
 {
 	double myX, myY, mySpeed, myAngle;
+	double oldX, oldY;
 	int myColor;
 
 	OddballParticle()
@@ -124,38 +163,49 @@ class OddballParticle implements Particle
 		myColor = color(255,255,255);
 		myX = width/2;
 		myY = height/2;
-		mySpeed = (Math.random()*8)+(Math.random()*3);
+		mySpeed = (Math.random()*3)+(Math.random()*4);
 		myAngle = Math.random()*2*Math.PI;
+	}
+
+	public void redo()
+	{
+
 	}
 
 	public void move()
 	{
-		if(Math.random() > 0.5f)
+		oldX = myX;
+		oldY = myY;
+
+		myX += (2*(Math.cos(myAngle) * mySpeed)) - (Math.cos(myAngle) * mySpeed);
+		myY += (2*(Math.sin(myAngle) * mySpeed)) - (Math.sin(myAngle) * mySpeed);
+
+		if(frameCount%15 == 0)
 		{
-			myX += ( Math.cos(myAngle) * mySpeed);
+			myAngle = Math.random()*2*Math.PI;
 		}
-		else
+
+		if(myX > width || myX < 0 )
 		{
-			myX -= ( Math.cos(myAngle) * mySpeed);
+			myAngle = Math.PI - myAngle;
 		}
-		if(Math.random() > 0.5f)
+		if(myY > height || myY < 0)
 		{
-			myY += ( Math.sin(myAngle) * mySpeed);
+			myAngle = -1 * myAngle;
 		}
-		else
-		{
-			myY -= ( Math.sin(myAngle) * mySpeed);
-		}
-		myAngle = Math.random()*2*Math.PI;
 	}
 
 	public void show()
 	{
 		noStroke();
 		fill(myColor);
-		ellipse((float)myX,(float)myY,7,7);
+		ellipse((float)myX,(float)myY,10,10);
+
+		// strokeWeight(10);
+		// stroke(myColor);
+		// line((float)oldX,(float)oldY,(float)myX,(float)myY);
 	}
-} //the pink bar was bothering me
+}
 
 
   static public void main(String[] passedArgs) {
